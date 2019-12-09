4dfb1c645d84cb61bc6b31e7cd2f4f39c13bccb5
Nikita Andrianov <diablo3nekit@mail.ru>
2019-12-09 21:19:59 +0400

    ver 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { View, EventSettingsModel } from '@syncfusion/ej2-angular-schedule';
+import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<ejs-schedule [currentView]="setView" [eventSettings]="eventObject" [views]="setViews"></ejs-schedule>',
+  //templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'calendar-project';
+  public setView: View = 'Month';
+  public setViews: View[] = ["Day", "Month"];
+  // public setDate: Date = new Date(2019, 11, 5);
+  // private eventDate: DataManager = new DataManager ({
+  // 	url: 'https://js.syncfusion.com/demos/ejservices/api/Schedule/LoadData',
+  // 	adaptor: new WebApiAdaptor,
+  // 	crossDomain: true
+  // });
+  // public eventObject: EventSettingsModel = {
+  // 	dataSource: this.eventDate
+  // }
+  public eventObject: EventSettingsModel = {
+  	dataSource: [{
+  		EventTitle: "Test",
+  		EventStart: new Date(2019, 11, 5, 4, 0),
+  		EventEnd: new Date(2019, 11, 5, 6, 0)
+  	}],
+  	fields: {
+  		subject: { name: 'EventTitle', default: "Pls, Enter Event Name!", title: "Enter Title"},
+  		startTime: { name: 'EventStart'},
+  		endTime: { name: 'EventEnd'}
+  	}
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ScheduleModule, RecurrenceEditorModule, DayService, WeekService, WorkWeekService, MonthService, MonthAgendaService, DragAndDropService, ResizeService } from '@syncfusion/ej2-angular-schedule';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ScheduleModule, RecurrenceEditorModule
   ],
-  providers: [],
+  providers: [DayService, WeekService, WorkWeekService, MonthService, MonthAgendaService, DragAndDropService, ResizeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
